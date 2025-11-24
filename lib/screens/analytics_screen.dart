55b05a6acb866463c33a01990386786ebb7148fc
Sanjay Siva <realsanjaysiva@gmail.com>
2025-11-24 22:50:43 +0530

    url fix

--- lib/screens/analytics_screen.dart
+++ lib/screens/analytics_screen.dart
@@ -187,7 +187,10 @@ class RecentTestData {
 // =================================================================================
 
 class _AnalyticsApiService {
-  static const String _baseUrl = "http://127.0.0.1:8000";
+  static const String _baseUrl = String.fromEnvironment(
+    'API_BASE_URL',
+    defaultValue: 'http://127.0.0.1:8000',
+  );
 
   static Future<AnalyticsResponse> fetchAnalyticsData(String authToken) async {
     final response = await http.get(
